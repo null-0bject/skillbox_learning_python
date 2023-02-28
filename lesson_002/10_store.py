3c019af7aa744c8a1afa35c39fabfb82f062f16c
null.object <monika22limonix@gmail.com>
2023-02-28 05:34:15 +0300

    ALL work is done!

--- lesson_002/10_store.py
+++ lesson_002/10_store.py
@@ -31,35 +31,37 @@ store = {
     ],
 }
 
-# Рассчитать на какую сумму лежит каждого товара на складе
-# например для ламп
-
-lamps_cost = store[goods['Лампа']][0]['quantity'] * store[goods['Лампа']][0]['price']
-# или проще (/сложнее ?)
-lamp_code = goods['Лампа']
-lamps_item = store[lamp_code][0]
-lamps_quantity = lamps_item['quantity']
-lamps_price = lamps_item['price']
-lamps_cost = lamps_quantity * lamps_price
-print('Лампа -', lamps_quantity, 'шт, стоимость', lamps_cost, 'руб')
-
 # Вывести стоимость каждого товара на складе: один раз распечать сколько всего столов, стульев и т.д. на складе
 # Формат строки <товар> - <кол-во> шт, стоимость <общая стоимость> руб
 
 # WARNING для знающих циклы: БЕЗ циклов. Да, с переменными; да, неэффективно; да, копипаста.
 # Это задание на ручное вычисление - что бы потом понять как работают циклы и насколько с ними проще жить.
 
-# TODO здесь ваш код
+lamps_count = store[goods['Лампа']][0]['quantity']
+lamps_cost = lamps_count * store[goods['Лампа']][0]['price']
 
-##########################################################################################
-# ВНИМАНИЕ! После того как __ВСЯ__ домашняя работа сделана и запушена на сервер,         #
-# нужно зайти в ЛМС (LMS - Learning Management System ) по адресу http://go.skillbox.ru  #
-# и оформить попытку сдачи ДЗ! Без этого ДЗ не будет проверяться!                        #
-# Как оформить попытку сдачи смотрите видео - https://youtu.be/qVpN0L-C3LU               #
-##########################################################################################
+table_count = store[goods['Стол']][0]['quantity'] + store[goods['Стол']][1]['quantity']
 
+table_cost = store[goods['Стол']][0]['quantity'] * store[goods['Стол']][0]['price'] \
+            + store[goods['Стол']][1]['quantity'] * store[goods['Стол']][1]['price']
 
 
+sofa_count = store[goods['Диван']][0]['quantity'] + store[goods['Диван']][1]['quantity']
+
+sofa_cost = store[goods['Диван']][0]['quantity'] * store[goods['Диван']][0]['price'] \
+            + store[goods['Диван']][1]['quantity'] * store[goods['Диван']][1]['price']
 
 
+chair_count = store[goods['Стул']][0]['quantity'] + store[goods['Стул']][1]['quantity'] \
+              + store[goods['Стул']][2]['quantity']
 
+chair_cost = store[goods['Стул']][0]['quantity'] * store[goods['Стул']][0]['price'] \
+            + store[goods['Стул']][1]['quantity'] * store[goods['Стул']][1]['price'] \
+            + store[goods['Стул']][2]['quantity'] * store[goods['Стул']][2]['price']
+
+
+if __name__ == '__main__':
+    print(f'Ламп - {lamps_count} шт, стоимость {lamps_cost} руб\n'
+          f'Столов - {table_count} шт, стоимость {table_cost} руб\n'
+          f'Диванов - {sofa_count} шт, стоимость {sofa_cost} руб\n'
+          f'Стульев - {chair_count} шт, стоимость {chair_cost} руб')
